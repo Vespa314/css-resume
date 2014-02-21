de8e7e5e88167b383ccc4dd108d24214ba1138ca
Vespa <kylensherlock@gmail.com>
2014-02-22 01:42:54 +0800

    code

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ css-resume
 ==========
 
 CSS实现简历
+
+参见：http://www.kylen314.com/archives/5310
+
+演示demo：http://www.kylen314.com/lab/resume/resume.php
---
+++ resume.php
@@ -0,0 +1,425 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>My Resume Demo</title>
+    <style type="text/css">
+div.paper{
+width:900px;
+margin: auto;
+background-color:#FFF;
+}
+
+div.baner{background-color:#AD292E;}
+
+div.baner2{
+background-color:#E7E6DE;
+position: relative;
+}
+
+img.avatar{
+border:10px solid #E7E6DE;
+float:left;
+}
+
+div.maininfo{
+position: relative;
+left: 30px;
+top: 15px;
+}
+
+#name{
+color: #FFF;
+font-size: 30px;
+font-weight: 100;
+position: relative;
+font-family: fantasy;
+top: 40px;
+left: 20px;
+}
+
+.email:before,.website:before,.school:before{
+ position: relative;
+ top: 6px;
+ padding-right: 5px;
+}
+
+.education:before,.experience:before,.project:before,.skill:before{
+ position: relative;
+ top: 3px;
+ padding-right: 10px;
+}
+
+.email:before{content:url(./mail_b.png);}
+.website:before{content:url(./website_b.png);}
+.school:before{content:url(./school_b.png);}
+.education:before{content:url(./education_b.png);}
+.experience:before{content:url(./experience_b.png);}
+.project:before{content:url(./project_b.png);}
+.skill:before{content:url(./skill_b.png);}
+
+.contact_info{
+float:left;
+position: relative;
+left: 10px;
+top: 45px;
+font-size: 15px;
+font-family: cursive;
+margin-right: 8px;
+color:#757699;
+}
+
+#main
+{
+position: relative;
+padding-top: 1px;
+float:left;
+width:100%;
+}
+
+.edu_exp_info{
+position: relative;
+font-size: 30px;
+font-family: fantasy;
+color: #AD292E;
+padding-left: 200px;
+line-height: 100%;
+margin-top: 30px;
+}
+
+.edu_item{
+position:relative;
+clear: both;
+width: 100%;
+float: left;
+}
+
+.edu_item span{
+display:block;
+}
+
+.whenwhere{
+font-size: 15px;
+font-family: cursive;
+color: #757699;
+width: 180px;
+float: left;
+padding-left: 34px;
+border-right: solid 3px #E7E6DE;
+padding-top: 20px;
+}
+
+.graycir{
+border-radius: 50%;
+border-color: #E7E6DE;
+width: 20px;
+height: 20px;
+background-color: #E7E6DE;
+position: relative;
+left: 205px;
+top: 20px;
+}
+
+.edu_detail{
+float: left;
+padding-left: 60px;
+border-left: solid 3px #E7E6DE;
+position: relative;
+left: -3px;
+margin-bottom: -5px;
+}
+
+.detail1{
+font-size: 20px;
+font-family: fantasy;
+}
+
+.detail2{
+font-size: 20px;
+font-family: Arial;
+color:#AD292E;
+}
+
+.detail3{
+font-family: Arial;
+color: #757699;
+list-style-type: circle;
+margin-top: 0px;
+margin-left: -20px;
+}
+
+.footer{
+background-color:#E7E6DE;
+float: left;
+width:100%;
+padding-bottom: 20px;
+}
+
+.project_area{float:left;}
+
+.skill_area{
+float:left;
+margin-left: 20px;
+}
+
+.pro_skill_info{
+font-size: 30px;
+font-family: fantasy;
+color: #AD292E;
+padding-left: 30px;
+position: relative;
+top: 5px;
+}
+
+.footer_list{
+border-left: solid 3px #757679;
+position: relative;
+left: 43px;
+padding-left: 20px;
+}
+
+.footer_item{}
+
+.graycir2{
+border-radius: 50%;
+border-color: #757679;
+width: 20px;
+height: 20px;
+background-color: #757679;
+position: relative;
+left: -31px;
+top: 20px;
+clear: both;
+}
+
+.project1,.project2,.skill1{font-family: Arial;}
+
+.project2{color: #757699;}
+
+.skillcir{
+border: solid 1px #757679;
+border-radius: 50%;
+width: 15px;
+height: 15px;
+
+float: left;
+margin-left: 5px;
+position: relative;
+top: 2px;
+}
+
+.skill1{
+float: left;
+width:90px;
+padding-left: 20px;
+}
+
+a{
+text-decoration:none;
+color:#757699;
+}
+</style>
+  </head>
+
+  <body style="background-color:#CCC;">
+
+<div class="paper">
+<div class="baner2" style="height:135px;">
+  <div class="baner" style="height:92px;">
+    <div class="maininfo">
+    <img class="avatar" src="./touxiang2.png" >
+    <div id="name">VESPA KYLEN</div>
+    <div class="email contact_info">kylensherlock@gmail.com</div>
+    <div class="website contact_info">
+    <a href="http://www.kylen314.com">www.kylen314.com</a>
+    </div>
+    <div class="school contact_info">ZheJiang University ISEE</div>
+  </div><!--end maininfo-->
+</div><!--end baner-->
+</div><!--end baner2-->
+
+<div id="main" style="background-color:#FFF;">
+  <div class="education edu_exp_info">EDUCATION</div>
+
+  <div class="edu_item">
+
+    <div class="whenwhere">
+      <span>09.2008-06.2012</span>
+      <span>GuangDong</span>
+    </div>
+
+    <div class="graycir"></div>
+
+    <div class="edu_detail">
+        <span class="detail1">South China University of Technology</span>
+        <span class="detail2">EI GPA:xx/4</span>
+        <ol class="detail3">
+          <li>The main responsible person the national innovation project</li>
+          <li>Mathematical Contest in Modeling XXX prize</li>
+        </ol>
+    </div>
+  </div><!--end edu_item-->
+
+  <div class="edu_item">
+
+  <div class="whenwhere">
+    <span>09.2012-03.2015</span>
+    <span>ZheJiang</span>
+  </div>
+ 
+  <div class="graycir"></div>
+
+  <div class="edu_detail">
+    <span class="detail1">ZheJiang University</span>
+    <span class="detail2">ISEE GPA:xx/4</span>
+    <ol class="detail3">
+      <li>Undergraduate Mathematical Contest in Modeling XXX prize</li>
+      <li>Authorized a software copyright on household ele-power analysis</li>
+    </ol>
+  </div>
+  </div><!--end edu_item-->
+
+
+<div class="experience edu_exp_info">EXPERIENCE</div>
+<div class="edu_item">
+
+    <div class="whenwhere">
+      <span>07.2011-09.2011</span>
+      <span>GuangDong</span>
+    </div>
+
+    <div class="graycir"></div>
+
+    <div class="edu_detail">
+        <span class="detail1">Tencent</span>
+        <span class="detail2">Weibo Apartment</span>
+        <ol class="detail3">
+          <li>Developed a tool for forwarding package</li>
+          <li>Participate an online project for lottering in QQ weibo</li>
+        </ol>
+    </div>
+  </div><!--end edu_item-->
+
+ <div class="edu_item">
+
+    <div class="whenwhere">
+      <span>XX.XXXX-XX.XXXX</span>
+      <span>XXXXXXXXX</span>
+    </div>
+
+    <div class="graycir"></div>
+
+    <div class="edu_detail">
+        <span class="detail1">XXXXXXXX</span>
+        <span class="detail2">XXXXXXXX</span>
+        <ol class="detail3">
+          <li>XXXXXXXXXXXXXXXXXXXXXXXXXXXX</li>
+          <li>XXXXXXXXXXXXXXXXXXXXXXXXXXXX</li>
+          <li>XXXXXXXXXXXXXXXXXXXXXXXXXXXX</li>
+        </ol>
+    </div>
+  </div><!--end edu_item-->
+  
+   <div class="edu_item">
+
+    <div class="whenwhere">
+      <span>XX.XXXX-XX.XXXX</span>
+      <span>XXXXXXXXX</span>
+    </div>
+
+    <div class="graycir"></div>
+
+    <div class="edu_detail">
+        <span class="detail1">XXXXXXXX</span>
+        <span class="detail2">XXXXXXXX</span>
+        <ol class="detail3">
+          <li>XXXXXXXXXXXXXXXXXXXXXXXXXXXX</li>
+          <li>XXXXXXXXXXXXXXXXXXXXXXXXXXXX</li>
+          <li>XXXXXXXXXXXXXXXXXXXXXXXXXXXX</li>
+        </ol>
+    </div>
+  </div><!--end edu_item-->
+</div><!--end main-->
+
+<div class="footer">
+<div class="project_area">
+<div class="project pro_skill_info">PROJECT</div>
+<div class="footer_list">
+
+ <div class="graycir2"></div>
+ <span class="footer_item">
+ <span class="project1">Algorithm Research</span>
+ <span class="project2">Vision based gesture tracking system</span>
+ </span>
+ 
+<div class="graycir2"></div>
+<span class="footer_item">
+<span class="project1">Software Development</span>
+ <span class="project2">Attendance System for ISEE</span></span>
+ 
+ 
+<div class="graycir2"></div>
+<span class="footer_item">
+<span class="project1">UI</span>
+ <span class="project2">XXXXXXXXXXXXXXXXXXXXXX</span></span>
+ 
+ <div class="graycir2"></div>
+<span class="footer_item">
+<span class="project1">Android APP</span>
+ <span class="project2">XXXXXXXXXXXXXXXXXXXXXX</span></span>
+ 
+  <div class="graycir2"></div>
+<span class="footer_item">
+<span class="project1">VI</span>
+ <span class="project2">XXXXXXXXXXXXXXXXXXXXXX</span></span>
+ 
+  <div class="graycir2"></div>
+<span class="footer_item">
+<span class="project1">Market Research</span>
+ <span class="project2">XXXXXXXXXXXXXXXXXXXXXX</span></span>
+ 
+ 
+</div><!--end footer_list-->
+</div><!--end project_area-->
+
+<div class="skill_area">
+<div class="skill pro_skill_info">SKILL & HOBBY</div>
+<div class="footer_list">
+
+<?php
+function skilllevel($level)
+  {
+    for($i=0;$i<$level;$i++)
+  	echo "<span class='skillcir' style='background-color: #757679;'></span>";
+    for($i=$level;$i<3;$i++)
+  	echo "<span class='skillcir'></span>";	
+  }
+  
+$skill=array("C++","Python","Matlab","Mathematica","CSS","HTML","PHP","JS","Japanese","English","Cantonese","Mandarin");
+$skilllevel=array(3,2,3,2,2,1,2,1,2,2,3,3);
+$skill_c=count($skill);
+for($i=0;$i<$skill_c;)
+{
+   echo "<div class='graycir2'></div>";
+   echo "<span class='footer_item'>";
+   echo "<span class='skill1'>".$skill[$i]."</span>";
+   skilllevel($skilllevel[$i]);
+   $i++;
+   if($i<$skill_c)
+   {
+   echo "<span class='skill1'>".$skill[$i]."</span>";
+   skilllevel($skilllevel[$i]);
+   }
+    $i++;
+    echo "</span>";
+}
+?>
+
+</div><!--end footer_list-->
+</div><!--end project_area-->
+</div><!--end footer-->
+
+<div class="baner" style="height:20px; float:left; width:100%"></div>
+</div><!--end paper-->
+
+</body></html>
